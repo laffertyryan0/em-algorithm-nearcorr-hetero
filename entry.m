34c09cfaa9cef6219408333a003a9f40aaf25077
ryan <laffertyryan0@gmail.com>
2026-01-22 20:14:16 -0500

    simulate subject level data

--- entry.m
+++ entry.m
@@ -4,19 +4,7 @@
 k = 200;
 % We have L labs 
 L = 300;
-% Each lab has a state vector Gamma_l which is a one hot binary vector
-% Gamma_l,j = 1 iff lab is in state j. Let's say j = 1...r, maybe r=2 
-r = 2;
-Gamma = zeros(L,r);
-alpha = ones(r,1)/r; % probability of each state
-randmemb = randsample(1:r,L,true,alpha)'; % Choose a random member 
-                       % of each row
-                       % That is for each lab choose a state
 
-idx = sub2ind(size(Gamma),(1:L)',randmemb); % Create indices for assignment
-Gamma(idx) = 1; % Assign the randomly chosen states to 1
-% We have L labs 
-L = 300;
 % Each lab has a state vector Gamma_l which is a one hot binary vector
 % Gamma_l,j = 1 iff lab is in state j. Let's say j = 1...r, maybe r=2 
 r = 2;
@@ -27,7 +15,11 @@ Gamma = mnrnd(1,alpha,L); % multinomial dist with n=1 is categorical dist
                           % Gamma is Lxr with each row a One Hot Encoded
                           % lab state
 
-% Each lab recruits N_l patients, all with the same state gamma_l
+
+% Number of subjects in lab l is n_subj(l)
+n_subjects = ones(L,1)*100;  % In this case assume 100 per lab
+
+% Each lab recruits n_subjects(l) patients, all with the same state gamma_l
 % Why? because gamma_l is specific to a (say) regional population,
 % and suppose any two individuals who could be recruited by the same
 % lab share this regional characteristic gamma_l (e.g. regional
@@ -42,10 +34,26 @@ Gamma = mnrnd(1,alpha,L); % multinomial dist with n=1 is categorical dist
 % To keep things simple, consider all the data to be mean 0, var 1
 % It shouldn't affect the results since we are only looking at correlations
 
-rho_state = {}; % cell array where rho_state{i} is the kxk rho matrix for 
-                % the ith state
+rho_state = {}; % cell array where rho_state{j} is the kxk rho matrix for 
+                % the jth state
 
-for i=1:r
-    rho_state{i} = randomCorrelationMatrix(sz);
+for j=1:r
+    rho_state{j} = randomCorrelationMatrix(k); % k = number of metabolites
+    assert(min(eig(rho_state{j}))>= 0, ...
+        "Non-PSD matrix found for simulation rho_i.")
+end
+
+% Generate subject level data for each lab
+subject_data = {}; % subject_data{i} is design matrix for 
+                         % ith lab
+for l=1:L
+    mu = zeros(1,k);                   % Centered mean for simplicity
+    lab_state = find(Gamma(l,:));      % Latent state for this lab = 
+                                       % index of the 1 column for l'th lab
+    Sigma = rho_state{lab_state};      % Variances all one, so Sigma = rho
+    subject_data{l} = mvnrnd(mu,...
+                             Sigma, ...
+                             n_subjects(l));
+end
 
 %% Using simulated data (lab aggregates only), estimate correlation matrix
